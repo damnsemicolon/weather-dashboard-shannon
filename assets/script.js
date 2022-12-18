322fbf9206749b0599bcd517d6589e225eb9b175
damnsemicolon <shannoninengland@gmail.com>
2022-12-18 23:09:01 +0000

    added CSS folder for polishing

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+
+ // Initial array of histories
+ var histories = ["London", "New York", "Hong Kong"];
+
+ // Function for displaying history data
+ function renderButtons() {
+
+   // YOUR CODE GOES HERE
+   $('#history-view').empty();
+for (var i = 0; i < histories.length; i++) {
+  var buttonEl = $('<button>');
+  buttonEl.addClass('history');
+  buttonEl.attr('data-name', histories[i]);
+  buttonEl.text(histories[i]);
+  $('#history-view').append(buttonEl);
+}
+}
+
+ // This function handles events where one button is clicked
+ $("#add-history").on("click", function(event) {
+
+  event.preventDefault();
+
+  var history = $('#history-input').val().trim();
+  histories.push(history);
+
+  renderButtons();
+
+ });
+
+ // Calling the renderButtons function to display the initial list of histories
+ renderButtons();
+
+
+ 
