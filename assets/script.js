1f8617637f69ce6edbfd374612cc103d4c00ebaa
damnsemicolon <shannoninengland@gmail.com>
2022-12-26 22:43:49 +0000

    scripted to display icon of weather for today

--- assets/script.js
+++ assets/script.js
@@ -29,43 +29,6 @@ $("#add-history").on("click", function (event) {
 // Calling the renderButtons function to display the initial list of histories
 renderButtons();
 
-// //  =================
-// //  ==== API KEY ====
-// //  =================
-
-// var APIKey = "b8ab8ad20877efa01fd8d4e1f506d0f7";
-
-// // URL for querying the database
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=hong%20kong&units=metic&appid=" + APIKey;
-
-// // Here we run our AJAX call to the OpenWeatherMap API
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// })
-//   // We store all of the retrieved data inside of an object called "response"
-//   .then(function (response) {
-
-//     // Log the queryURL
-//     console.log(queryURL);
-
-//     // Log the resulting object
-//     console.log(response);
-
-//     // Transfer content to HTML
-//     $(".result-city").html("<h1>" + response.city.name + " Weather Details</h1>");
-//     // Convert the temperature to Celsius
-//     var tempC = response.list[0].main.temp - 273.15;
-//     // add temperature content to html
-//     $(".result-tempC").text("Temperature (C) " + tempC.toFixed(1) + "°C");
-//     // add humidity content to html
-//     $(".result-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
-//     // add wind speed content to html
-//     $(".result-wind").text("Wind Speed: " + response.list[0].wind.speed + " KPH");
-
-//   });
-
 function displaycityweather() {
   var history = $(this).attr("Data-name");
   // API key
@@ -80,23 +43,29 @@ $.ajax({
   method: "GET"
 }).then(function (response) {
 
-    // Log the queryURL
-    console.log(queryURL);
-
     // Log the resulting object
     console.log(response);
 
     // Transfer content to HTML
-    $(".result-city").html("<h1>" + response.city.name + " Weather Details</h1>");
+    var responseDate = response.list[0].dt_txt
+    var responseToday = responseDate.split(" ");
+    
+    $(".result-city").html("<h1>" + response.city.name + " Weather Details (" + responseToday[0] +")" +"</h1>");
+  
     // Convert the temperature to Celsius
-    var tempC = response.list[0].main.temp - 273.15;
+    var tempCLow = response.list[2].main.temp - 273.15;
+    var tempCHigh = response.list[5].main.temp - 273.15;
     // add temperature content to html
-    $(".result-tempC").text("Temperature (C) " + tempC.toFixed(1) + "°C");
+    $(".result-tempC").text("Temperature: " + tempCLow.toFixed(1) + "°C - " + tempCHigh.toFixed(1) + "°C");
     // add humidity content to html
     $(".result-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
     // add wind speed content to html
     $(".result-wind").text("Wind Speed: " + response.list[0].wind.speed + " KPH");
 
+    // Icon
+    var iconcode = response.list[0].weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#wicon').attr('src', iconurl);
   });
 }
  // Adding a click event listener to all elements with a class of "history-btn"
