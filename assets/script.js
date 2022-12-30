4fa151d889ea8a6ff9e4c0ba369566ba9660ad9f
damnsemicolon <shannoninengland@gmail.com>
2022-12-30 19:38:09 +0000

    polished layout with CSS

--- assets/script.js
+++ assets/script.js
@@ -68,33 +68,60 @@ function displaycityweather() {
     var iconcode = response.weather[0].icon;
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
     $('#wicon').attr('src', iconurl);
-  }) 
+  })
   // FIVE-DAY FORECAST
   $.ajax({
     url: queryURL2,
     method: "GET"
   }).then(function (response2) {
-  // for loop to display 5 day forecast into cards
+    // for loop to display 5 day forecast into cards
     for (var i = 0; i < 5; i++) {
-  var day = response2.list[i * 8];
-  var responseDate = day.dt_txt;
-  var responseToday = responseDate.split(" ");
-  // Date
-  $(".result-city-" + (i + 1)).html("<h5>" + responseToday[0] + "</h5>");
-  // Icon
-  var iconcode = day.weather[0].icon;
-  var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-  $('#wicon-' + (i + 1)).attr('src', iconurl);
-  // Temp
-  var tempC = day.main.temp - 273.15;
-  $(".result-tempC-" + (i + 1)).text("Temp: " + tempC.toFixed(1) + "°C");
-  // Humidity
-  $(".result-humidity-" + (i + 1)).text("Humidity: " + day.main.humidity + "%");
-  // Wind
-  $(".result-wind-" + (i + 1)).text("Wind: " + day.wind.speed + " KPH");
-}
+      var day = response2.list[i * 8];
+      var responseDate = day.dt_txt;
+      var responseToday = responseDate.split(" ");
+      // Date
+      $(".result-city-" + (i + 1)).html("<h5>" + responseToday[0] + "</h5>");
+      // Icon
+      var iconcode = day.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      $('#wicon-' + (i + 1)).attr('src', iconurl);
+      // Temp
+      var tempC = day.main.temp - 273.15;
+      $(".result-tempC-" + (i + 1)).text("Temp: " + tempC.toFixed(1) + "°C");
+      // Humidity
+      $(".result-humidity-" + (i + 1)).text("Humidity: " + day.main.humidity + "%");
+      // Wind
+      $(".result-wind-" + (i + 1)).text("Wind: " + day.wind.speed + " KPH");
+    }
 
   });
 }
 // Adding a click event listener to all elements with a class of "history-btn"
 $(document).on("click", ".history-btn", displaycityweather);
+
+// Get the search form
+const searchForm = document.querySelector('form');
+
+// Add an event listener to the form
+searchForm.addEventListener('.add-history', function(event) {
+  // Prevent the form from being submitted
+  event.preventDefault();
+
+  // Get the value of the search input
+  const searchTerm = searchInput.value;
+
+  // Check if local storage is supported by the browser
+  if (typeof(Storage) !== "undefined") {
+    // Get the existing search history from local storage
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    // Add the new search term to the search history
+    searchHistory.push(searchTerm);
+
+    // Update the search history in local storage
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+    // Display the search history
+    displaySearchHistory();
+  }
+});
